204249c70c9881cbbbaea708d724206b8ba14971
matheus <grafit933@gmail.com>
2023-08-14 15:09:56 -0300

    up

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/forum', [SiteController::class, 'forum']);
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
